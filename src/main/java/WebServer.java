4bdaee184be7ce134e78bf1e7d7e6bb3e492ec97
gad969 <gad969@gmail.com>
2022-08-20 17:43:57 +0500

    Add new line

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -2,5 +2,6 @@ public class WebServer {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("WTF???");
+        System.out.println("AAAA, I did it!!!!!!!");
     }
 }
